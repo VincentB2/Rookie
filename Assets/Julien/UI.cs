bfe5144fea293d432892f080b17d2285cab5e5c9
jqntn <jqntn88@gmail.com>
2020-11-02 15:56:49 +0100

    adding ui

--- Assets/Julien/UI.cs
+++ Assets/Julien/UI.cs
@@ -3,28 +3,39 @@ using UnityEngine;
 using UnityEngine.UI;
 class UI : MonoBehaviour
 {
-    public bool isGamePaused;
+    public bool isGameMenued, isGamePaused;
+    public float timeScale;
     GameObject MenuLayer, InGameLayer, PauseLayer;
-    Button PauseButton, ResumeButton;
+    Button PlayButton, PauseButton, ResumeButton, MenuButton, QuitButton;
     void Awake()
     {
+        // Layers
         MenuLayer = GameObject.Find("MenuLayer");
         InGameLayer = GameObject.Find("InGameLayer");
         PauseLayer = GameObject.Find("PauseLayer");
+        // Buttons
+        PlayButton = GameObject.Find("PlayButton").GetComponent<Button>();
         PauseButton = GameObject.Find("PauseButton").GetComponent<Button>();
         ResumeButton = GameObject.Find("ResumeButton").GetComponent<Button>();
-        PauseButton.onClick.AddListener(PauseGame);
-        ResumeButton.onClick.AddListener(PauseGame);
+        MenuButton = GameObject.Find("MenuButton").GetComponent<Button>();
+        QuitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+        // Listeners
+        PlayButton.onClick.AddListener(MenuGame);
+        PauseButton.onClick.AddListener(PauseResumeGame);
+        ResumeButton.onClick.AddListener(PauseResumeGame);
+        MenuButton.onClick.AddListener(MenuGame);
+        QuitButton.onClick.AddListener(QuitGame);
     }
     void Update()
     {
-        Time.timeScale = isGamePaused ? 0 : 1;
-        InGameLayer.SetActive(!isGamePaused);
-        PauseLayer.SetActive(isGamePaused);
-        if (Input.GetKeyDown(KeyCode.Escape)) PauseGame();
+        timeScale = Time.timeScale;
+        Time.timeScale = isGameMenued ? 0 : isGamePaused ? 0 : 1;
+        MenuLayer.SetActive(isGameMenued);
+        InGameLayer.SetActive(!isGameMenued && !isGamePaused);
+        PauseLayer.SetActive(!isGameMenued && isGamePaused);
+        if (Input.GetKeyDown(KeyCode.Escape)) PauseResumeGame();
     }
-    void PauseGame()
-    {
-        isGamePaused ^= true;
-    }
+    void PauseResumeGame() { isGamePaused ^= true; }
+    void MenuGame() { isGameMenued ^= true; PauseResumeGame(); }
+    void QuitGame() { Application.Quit(); }
 }
