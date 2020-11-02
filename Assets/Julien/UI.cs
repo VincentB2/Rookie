5b3cb62e6d872bb5ace34734da2525d21da5eb56
jqntn <jqntn88@gmail.com>
2020-11-02 15:29:00 +0100

    Update Scene Julien

---
+++ Assets/Julien/UI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+class UI : MonoBehaviour
+{
+    public bool isGamePaused;
+    GameObject MenuLayer, InGameLayer, PauseLayer;
+    Button PauseButton, ResumeButton;
+    void Awake()
+    {
+        MenuLayer = GameObject.Find("MenuLayer");
+        InGameLayer = GameObject.Find("InGameLayer");
+        PauseLayer = GameObject.Find("PauseLayer");
+        PauseButton = GameObject.Find("PauseButton").GetComponent<Button>();
+        ResumeButton = GameObject.Find("ResumeButton").GetComponent<Button>();
+        PauseButton.onClick.AddListener(PauseGame);
+        ResumeButton.onClick.AddListener(PauseGame);
+    }
+    void Update()
+    {
+        Time.timeScale = isGamePaused ? 0 : 1;
+        InGameLayer.SetActive(!isGamePaused);
+        PauseLayer.SetActive(isGamePaused);
+        if (Input.GetKeyDown(KeyCode.Escape)) PauseGame();
+    }
+    void PauseGame()
+    {
+        isGamePaused ^= true;
+    }
+}
