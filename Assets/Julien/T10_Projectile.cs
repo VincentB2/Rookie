e7215881c0b3e62b8969438098bab6c63575068c
jqntn <jqntn88@gmail.com>
2020-11-03 17:40:07 +0100

    Updating EnemyShoot

---
+++ Assets/Julien/T10_Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+class T10_Projectile : MonoBehaviour
+{
+    void Awake()
+    {
+        Destroy(gameObject, 2);
+    }
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+}
