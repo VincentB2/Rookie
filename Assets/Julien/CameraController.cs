328401a5585dfd6c866621eea5ceea106829a321
jqntn <jqntn88@gmail.com>
2020-11-03 11:40:37 +0100

    adding camera smoothing

---
+++ Assets/Julien/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+class CameraController : MonoBehaviour
+{
+    UI ui;
+    // Camera
+    Camera camera;
+    Transform target;
+    Vector3 offset;
+    public float amplitudeInMenu = 2;
+    public float smoothInMenu = .1F;
+    public float smoothInGame = .1F;
+    void Awake()
+    {
+        ui = GameObject.Find("UI").GetComponent<UI>();
+        // Camera
+        camera = GetComponent<Camera>();
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        offset = transform.position;
+    }
+    void Update()
+    {
+        if (ui.isGameMenued || ui.isGamePaused) transform.position =
+            Vector3.Lerp(transform.position, new Vector3((Input.mousePosition.x - Screen.width / 2) / (amplitudeInMenu * 1000),
+                (Input.mousePosition.y - Screen.height / 2) / (amplitudeInMenu * 1000), offset.z), smoothInMenu);
+        else transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothInGame);
+    }
+}
--- Assets/Julien/Emoji.cs
+++ Assets/Julien/Emoji.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 class Emoji : MonoBehaviour
 {
     public enum Type
