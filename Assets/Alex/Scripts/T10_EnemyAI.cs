b6c894d54795929e78a7d50af1890913e5ecab71
jqntn <jqntn88@gmail.com>
2020-11-03 15:56:52 +0100

    adding enemy

--- Assets/Alex/Scripts/T10_EnemyAI.cs
+++ Assets/Alex/Scripts/T10_EnemyAI.cs
@@ -1,36 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
-
 public class T10_EnemyAI : MonoBehaviour
 {
     public GameObject player;
-
     [Header("EnemyCarac")]
     public float speedEnemy = 1.5f;
     public float rangeEnemy = 4.0f;
     public float lifeEnemy = 1.5f;
     public int damageEnemy = 1;
-
+    // Julien
+    // Look At
+    Transform target;
+    Vector3 thisPos;
+    Vector3 targetPos;
+    float angle;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        // Julien
+        // Look At
+        if (player)
+            target = player.GetComponent<Transform>();
     }
-
-
     void Update()
     {
         float step = speedEnemy * Time.deltaTime;
-
-        if((player.transform.position - transform.position).magnitude <= rangeEnemy) 
+        if ((player.transform.position - transform.position).magnitude <= rangeEnemy)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
         }
-
         EnemyDeath();
+        // Julien
+        if (gameObject.tag == "EnemyShoot")
+        {
+            // Look At
+            if (target)
+            {
+                thisPos = transform.position;
+                targetPos = target.position;
+                targetPos.x = targetPos.x - thisPos.x;
+                targetPos.y = targetPos.y - thisPos.y;
+                angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), Time.deltaTime * 4);
+            }
+        }
     }
-
     void EnemyDeath()
     {
         if (lifeEnemy <= 0)
@@ -38,8 +52,6 @@ public class T10_EnemyAI : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-
     public void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Player"))
@@ -48,6 +60,4 @@ public class T10_EnemyAI : MonoBehaviour
             Destroy(gameObject);
         }
     }
-
-    
 }
