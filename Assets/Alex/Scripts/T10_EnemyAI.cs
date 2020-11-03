6b002a0eb9d196a1444b4a02a12e3a42280a412e
Alex Copin <alexcopin@live.fr>
2020-11-03 19:22:21 +0100

    Tir et Dégâts et Pièges

--- Assets/Alex/Scripts/T10_EnemyAI.cs
+++ Assets/Alex/Scripts/T10_EnemyAI.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 public class T10_EnemyAI : MonoBehaviour
 {
     public GameObject player;
+    public enum EnemyType
+    {
+        NORMAL,
+        SMALL,
+        BIG,
+        SHOOT
+    }
+    public EnemyType enemyType;
     [Header("EnemyCarac")]
     public float speedEnemy = 1.5f;
     public float rangeEnemy = 4.0f;
@@ -17,9 +25,9 @@ public class T10_EnemyAI : MonoBehaviour
     // EnemyShoot
     [Header("EnemyShoot")]
     public GameObject enemyShootProjectile;
+    public float rangeEnemyShoot = 10.0f;
     public float enemyShootForce = 1,
-        enemyShootReactivity = 5,
-        enemyShootSpeed = 1,
+        enemyShootReactivity = 50f,
         enemyShootProjectileSpeed = 5,
         enemyShootFrequency = .5F;
     float timer;
@@ -33,11 +41,12 @@ public class T10_EnemyAI : MonoBehaviour
     }
     void Update()
     {
+
         // Julien
-        if (gameObject.tag == "EnemyShoot")
+        if (enemyType == EnemyType.SHOOT)
         {
             // Look At
-            if (target)
+            if ((player.transform.position - transform.position).magnitude <= rangeEnemy && (player.transform.position - transform.position).magnitude > rangeEnemyShoot)
             {
                 thisPos = transform.position;
                 targetPos = target.position;
@@ -45,17 +54,25 @@ public class T10_EnemyAI : MonoBehaviour
                 targetPos.y = targetPos.y - thisPos.y;
                 angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), Time.deltaTime * enemyShootReactivity);
-                transform.position = Vector2.MoveTowards(transform.position, target.position, enemyShootSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, target.position, speedEnemy * Time.deltaTime);
             }
-            // EnemyShoot
-            if (enemyShootProjectile)
+            else if ((player.transform.position - transform.position).magnitude <= rangeEnemyShoot)
             {
-                timer -= Time.deltaTime;
-                if (timer < 0)
+                thisPos = transform.position;
+                targetPos = target.position;
+                targetPos.x = targetPos.x - thisPos.x;
+                targetPos.y = targetPos.y - thisPos.y;
+                angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angle)), Time.deltaTime * enemyShootReactivity);
+                if (enemyShootProjectile)
                 {
-                    GameObject projectile = Instantiate(enemyShootProjectile, transform.position, transform.rotation);
-                    projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.right * enemyShootProjectileSpeed * 100);
-                    timer = enemyShootFrequency;
+                    timer -= Time.deltaTime;
+                    if (timer < 0)
+                    {
+                        GameObject projectile = Instantiate(enemyShootProjectile, transform.position, transform.rotation);
+                        projectile.GetComponent<Rigidbody2D>().AddForce(projectile.transform.right * enemyShootProjectileSpeed * 100);
+                        timer = enemyShootFrequency;
+                    }
                 }
             }
         }
@@ -77,12 +94,23 @@ public class T10_EnemyAI : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void RotateGameObject(Vector3 target, float RotationSpeed, float offset)
+    {
+        Vector3 dir = target - transform.position;
+        //get the angle from current direction facing to desired target
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //set the angle into a quaternion + sprite offset depending on initial sprite facing direction
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, angle + offset));
+        //Roatate current game object to face the target using a slerp function which adds some smoothing to the move
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, RotationSpeed * Time.deltaTime);
+    }
     public void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.tag == "Player")
         {
-            player.GetComponent<T10_PlayerFight>().playerHP -= damageEnemy;
-            Destroy(gameObject);
+            Debug.Log("Wtf");
+            col.gameObject.GetComponent<T10_PlayerFight>().playerHP -= damageEnemy;
+            lifeEnemy -= 1.5f;
         }
     }
 }
--- Assets/Julien/T10_Projectile.cs
+++ Assets/Julien/T10_Projectile.cs
@@ -8,6 +8,9 @@ class T10_Projectile : MonoBehaviour
     void OnTriggerEnter2D(Collider2D c)
     {
         if (c.gameObject.CompareTag("Player"))
+        {
+            c.gameObject.GetComponent<T10_PlayerFight>().playerHP -= 1;
             Destroy(gameObject);
+        }
     }
 }
