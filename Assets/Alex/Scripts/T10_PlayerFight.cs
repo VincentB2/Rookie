1c44c196a0d06defb8a512f108aa43dfea0349f4
jqntn <jqntn88@gmail.com>
2020-11-05 17:43:37 +0100

    updating ui

--- Assets/Alex/Scripts/T10_PlayerFight.cs
+++ Assets/Alex/Scripts/T10_PlayerFight.cs
@@ -11,12 +11,16 @@ public class T10_PlayerFight : MonoBehaviour
     public float timerGlobal;
     float timerScore;
     public GameObject textTimer;
+    // Julien
+    GameObject livesText;
     // Start is called before the first frame update
     void Awake()
     {
         playerHP = playerHPValue;
         timerGlobal = timerGlobalValue;
         textTimer = GameObject.Find("/UI/InGameLayer/timer");
+        // Julien
+        livesText = GameObject.Find("lives");
     }
     // Update is called once per frame
     void Update()
@@ -24,12 +28,13 @@ public class T10_PlayerFight : MonoBehaviour
         timerGlobal -= Time.deltaTime;
         timerScore = (timerGlobalValue - timerGlobal) * 1000;
         textTimer.GetComponent<TextMeshProUGUI>().text = "Time : " + timerGlobal;
+        // Julien
+        livesText.GetComponent<TextMeshProUGUI>().text = "Lives : " + playerHP;
         PlayerPrefs.SetFloat("ScoreTeam10", timerScore);
         if (playerHP <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-
     }
     public void TakeDamage(float damage)
     {
