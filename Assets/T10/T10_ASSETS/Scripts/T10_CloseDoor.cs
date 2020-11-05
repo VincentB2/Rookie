15d5c25022f33261cd5ecb1ebbcbb8c1d3006723
VincentB2 <hjklm96301@gmail.com>
2020-11-05 23:48:29 +0100

    Corrections portes finish

---
+++ Assets/T10/T10_ASSETS/Scripts/T10_CloseDoor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T10_CloseDoor : MonoBehaviour
+{
+    public T10_IntVariable enemiesDead;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<BoxCollider2D>().enabled = true;
+            GetComponent < CapsuleCollider2D>().enabled = false;
+            enemiesDead.Value = 0;
+        }
+    }
+}
--- Assets/T10/T10_ASSETS/Scripts/T10_Doors.cs
+++ Assets/T10/T10_ASSETS/Scripts/T10_Doors.cs
@@ -4,63 +4,46 @@ using UnityEngine;
 public class T10_Doors : MonoBehaviour
 {
     public T10_IntVariable enemiesDead;
-    public T10_Doors doors;
     public enum DOORS {UN, DEUX, TROIS, QUATRE, CINQ}
     public DOORS doorID;
-    public List<int> goal = new List<int>();
-    public List<bool> isOpen = new List<bool>();
+    public int goal;
 
 
     private void Update()
     {
         if (doorID == DOORS.UN)
         {
-            if (isOpen[0])
+            if (enemiesDead.Value >= goal)
             {
-                if (enemiesDead.Value >= goal[0])
-                {
-                    gameObject.SetActive(false);
-                }
+                gameObject.SetActive(false);
             }
         }
         else if (doorID == DOORS.DEUX)
         {
-            if (isOpen[1])
+            if (enemiesDead.Value == goal)
             {
-                if (enemiesDead.Value == goal[1])
-                {
-                    gameObject.SetActive(false);
-                }
+                gameObject.SetActive(false);
             }
         }
         else if (doorID == DOORS.TROIS)
         {
-            if (isOpen[2])
+            if (enemiesDead.Value == goal)
             {
-                if (enemiesDead.Value == goal[2])
-                {
-                    gameObject.SetActive(false);
-                }
+                gameObject.SetActive(false);
             }
         }
         else if (doorID == DOORS.QUATRE)
         {
-            if (isOpen[3])
+            if (enemiesDead.Value == goal)
             {
-                if (enemiesDead.Value == goal[3])
-                {
-                    gameObject.SetActive(false);
-                }
+                gameObject.SetActive(false);
             }
         }
         else if (doorID == DOORS.CINQ)
         {
-            if (isOpen[4])
+            if (enemiesDead.Value == goal)
             {
-                if (enemiesDead.Value == goal[4])
-                {
-                    gameObject.SetActive(false);
-                }
+                gameObject.SetActive(false);
             }
         }
 
