58e3c99f674af3899d83575c8a9e7d91515d2151
VincentB2 <hjklm96301@gmail.com>
2020-11-06 00:45:02 +0100

    re

--- Assets/T10/T10_ASSETS/Scripts/T10_Doors.cs
+++ Assets/T10/T10_ASSETS/Scripts/T10_Doors.cs
@@ -4,49 +4,14 @@ using UnityEngine;
 public class T10_Doors : MonoBehaviour
 {
     public T10_IntVariable enemiesDead;
-    public enum DOORS {UN, DEUX, TROIS, QUATRE, CINQ}
-    public DOORS doorID;
     public int goal;
 
 
     private void Update()
     {
-        if (doorID == DOORS.UN)
-        {
             if (enemiesDead.Value >= goal)
             {
                 gameObject.SetActive(false);
             }
-        }
-        else if (doorID == DOORS.DEUX)
-        {
-            if (enemiesDead.Value == goal)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        else if (doorID == DOORS.TROIS)
-        {
-            if (enemiesDead.Value == goal)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        else if (doorID == DOORS.QUATRE)
-        {
-            if (enemiesDead.Value == goal)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        else if (doorID == DOORS.CINQ)
-        {
-            if (enemiesDead.Value == goal)
-            {
-                gameObject.SetActive(false);
-            }
-        }
-
-
     }
 }
--- Assets/T10/T10_ASSETS/Scripts/T10_EnemySpawn.cs
+++ Assets/T10/T10_ASSETS/Scripts/T10_EnemySpawn.cs
@@ -9,27 +9,36 @@ public class T10_EnemySpawn : MonoBehaviour
     public float timeBeforeSpawnValue = 2.0f;
     private float timeBeforeSpawn;
     public GameObject enemyType;
+    public GameObject door;
+    public T10_IntVariable enemiesDead;
+    private int enemyToKill;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         timeBeforeSpawn = timeBeforeSpawnValue;
+        enemyToKill = door.GetComponent<T10_Doors>().goal;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((player.transform.position - transform.position).magnitude <= rangeSpawn)
+        if (enemiesDead.Value < enemyToKill)
         {
-            if(timeBeforeSpawn <= 0) 
+            if ((player.transform.position - transform.position).magnitude <= rangeSpawn)
             {
-                Instantiate(enemyType, transform.position, transform.rotation);
-                timeBeforeSpawn = timeBeforeSpawnValue;
-            }
-            else 
-            {
-                timeBeforeSpawn -= Time.deltaTime;
+                if (timeBeforeSpawn <= 0)
+                {
+                    Instantiate(enemyType, transform.position, transform.rotation);
+                    timeBeforeSpawn = timeBeforeSpawnValue;
+                }
+                else
+                {
+                    timeBeforeSpawn -= Time.deltaTime;
+                }
             }
         }
     }
 }
+
+
