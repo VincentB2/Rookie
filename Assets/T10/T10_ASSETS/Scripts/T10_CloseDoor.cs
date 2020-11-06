ac568562043a5435d6b8fce6f52b6e2dc331182f
VincentB2 <hjklm96301@gmail.com>
2020-11-06 01:08:58 +0100

    goal

--- Assets/T10/T10_ASSETS/Scripts/T10_CloseDoor.cs
+++ Assets/T10/T10_ASSETS/Scripts/T10_CloseDoor.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class T10_CloseDoor : MonoBehaviour
 {
     public T10_IntVariable enemiesDead;
+    public GameObject door;
+    public TextMeshProUGUI text;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -13,6 +16,7 @@ public class T10_CloseDoor : MonoBehaviour
             GetComponent<BoxCollider2D>().enabled = true;
             GetComponent < CapsuleCollider2D>().enabled = false;
             enemiesDead.Value = 0;
+            text.text = door.GetComponent<T10_Doors>().goal.ToString();
         }
     }
 }
